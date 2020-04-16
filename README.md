f30189f61d72e1002cc5bb8d02e2b93ea279cab3
Igor Magollo <igraphmagollo@gmail.com>
2020-04-16 20:19:01 -0300

    Initial commit + 1001

---
+++ README.md
@@ -0,0 +1,3 @@
+# URI on Scala
+
+O intuito deste repositório é o apredizado da linguagem Scala resolvendo problemas do URI
---
+++ iniciante/1001/Main.scala
@@ -0,0 +1,9 @@
+import scala.io.StdIn._
+
+object Main {
+    def main(args: Array[String]) {
+        val x: Int = readInt()
+        val y: Int = readInt()
+        println("X = " + (x + y))
+    }
+}
