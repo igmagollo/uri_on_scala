aa98c8b6f0a1f6a16b80ba96cdc634fd5937c20c
Igor Magollo <igraphmagollo@gmail.com>
2020-04-17 01:11:31 -0300

    + 1003

---
+++ iniciante/1003/Main.scala
@@ -0,0 +1,10 @@
+import scala.io.StdIn.readInt
+
+object Main {
+    def main(args: Array[String]) {
+        val a: Int = readInt()
+        val b: Int = readInt()
+        val soma: Int = a + b
+        println(f"SOMA = $soma")
+    }
+}
